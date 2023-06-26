0964c2f6e1799869f11a7a1a3e812f7e6f4db410
Youssef97dev <youssefmoussafi97@gmail.com>
2023-06-26 04:48:41 -1100

    finish Download Facture

--- src/hooks/dashboard/useDownload.js
+++ src/hooks/dashboard/useDownload.js
@@ -10,24 +10,24 @@ export const useDownload = () => {
   const download = async (id, type) => {
     setIsLoadingDownload(true);
 
-    try {
-      const response = await axios.post(`${DOWNLOAD}/${id}/${type}`, {
-        responseType: "stream",
+    axios({
+      url: `${DOWNLOAD}/${id}/${type}`,
+      method: "POST",
+      responseType: "blob", // important
+    })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "file.pdf"); //or any other extension
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((error) => {
+        setErrorDownload(error);
+        console.log(error);
+        setIsLoadingDownload(false);
       });
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", "file.pdf"); //or any other extension
-      document.body.appendChild(link);
-      link.click();
-      console.log(response);
-
-      setIsLoadingDownload(false);
-    } catch (error) {
-      setErrorDownload(error);
-      console.log(error);
-      setIsLoadingDownload(false);
-    }
   };
 
   return { download, errorDownload, isLoadingDownload };
